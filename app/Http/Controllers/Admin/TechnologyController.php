21db911c5573676fc9e042150e85d57bb9214c2c
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-07-12 15:23:20 +0200

    add functions in new controllers

--- app/Http/Controllers/Admin/TechnologyController.php
+++ app/Http/Controllers/Admin/TechnologyController.php
@@ -4,8 +4,72 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Technology;
+
 
 class TechnologyController extends Controller
 {
-    //
+    public function index()
+    {
+        $technology = Technology::paginate(5);
+
+        return view('admin.technologies.index', compact('technologies'));
+    }
+
+
+    public function create()
+    {
+        return view('admin.technologies.create', compact('technologies'));
+    }
+
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $newTechnology = new Technology();
+
+        $newTechnology->technology     = $data['technology'];
+        $newTechnology->save();
+
+
+        return to_route('admin.technologies.show', ['technologies' => $newTechnology]);
+    }
+
+
+    public function show(Technology $technology)
+    {
+        return view('admin.technologies.show', compact('technology'));
+    }
+
+
+    public function edit(Technology $technology)
+    {
+        return view('admin.technologies.edit', compact('technologies'));
+    }
+
+
+
+    public function update(Request $request, Technology $technology)
+    {
+        $data = $request->all();
+
+        $newTechnology = new Technology();
+
+        $newTechnology->technology     = $data['technology'];
+        $newTechnology->update();
+    }
+
+
+    public function destroy(Technology $technology)
+    {
+        foreach ($technology->project as $project) {
+            $project->technology_id = 1;
+            $project->update();
+        }
+
+        $technology->delete();
+
+        return to_route('admin.type.index')->with('delete_success', $technology);
+    }
 }
--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -4,8 +4,57 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Type;
 
 class TypeController extends Controller
 {
-    //
+    public function index()
+    {
+        $types = Type::all();
+        return view('admin.types.index', compact('types'));
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show(Type $type)
+    {
+        return view('admin.type.show', compact('type'));
+    }
+
+
+    public function edit(Type $type)
+    {
+        //
+    }
+
+
+
+    public function update(Request $request, Type $type)
+    {
+        //
+    }
+
+
+    public function destroy(Type $type)
+    {
+        foreach ($type->project as $project) {
+            $project->type_id = 1;
+            $project->update();
+        }
+
+        $type->delete();
+
+        return to_route('admin.type.index')->with('delete_success', $type);
+    }
 }
